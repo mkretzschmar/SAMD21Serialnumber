e751f50e6504e32e69e94fdb9e60619c6e93af99
Marek Kretzschmar <mkretzschmar@users.noreply.github.com>
2019-07-31 09:53:37 +0200

    Create SAMD21DeviceID.h

---
+++ SAMD21DeviceID.h
@@ -0,0 +1,12 @@
+#ifndef SAMD21_h
+#define SAMD21_h
+
+// Simple class that provides Methods for accessing the
+// SAMD21 128bit serial number
+class SAMD21 {
+ public:
+   SAMD21(); // initialize without params
+   void getSerialNumber(); // returns the unique DeviceID 
+ private:
+};
+#endif
