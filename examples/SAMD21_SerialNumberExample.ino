0d1b9768eaba1be718159241f0e20fd85aa0f788
Marek Kretzschmar <mkretzschmar@users.noreply.github.com>
2019-07-31 14:21:57 +0200

    Update SAMD21_SerialNumberExample.ino

--- examples/SAMD21_SerialNumberExample.ino
+++ examples/SAMD21_SerialNumberExample.ino
@@ -1,1 +1,18 @@
+#include <SAMD21DeviceID.h>
 
+SAMD21 samd = SAMD21();
+char serialnumber[33];
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+  samd.getSerialNumber(serialnumber);
+  delay(1000);
+  Serial.print("[SETUP] chip serial#: 0x");
+  Serial.println(serialnumber);
+}
+
+void loop() {
+  delay(1000);
+  Serial.println(serialnumber);
+}
